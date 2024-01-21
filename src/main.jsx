d15bb962f0e7a82d2ccf5b012449168b58121f13
geremeev <g.eremeev@cft.ru>
2024-01-22 00:47:34 +0700

    as on lecture state

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+/* eslint-disable react/no-children-prop */
+import ReactDOM from "react-dom/client";
+import { mocks } from "./constants/mock";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <div>
+    <button
+      id={"button_1" + 1}
+      onClick={() => console.log("Hello")}
+      onMouseEnter={() => console.log("Hello, Students!")}
+      onMouseLeave={() => console.log("Bye, Students!")}
+    >
+      <span>Click me!</span>
+    </button>
+    <button>
+      <span>Click me!</span>
+    </button>
+    <button>
+      <span>Click me!</span>
+    </button>
+
+    <div>
+      <div>
+        <p>Name: {mocks[0].name}</p>
+      </div>
+      <div>
+        <p>Name: {mocks[1].name}</p>
+      </div>
+    </div>
+  </div>
+);
+
+// root.render(
+//   React.createElement("div", {
+//     children: [
+//       React.createElement("button", {
+//         children: React.createElement("span", {
+//           children: "Click Me!",
+//         }),
+//         onClick: () => console.log("Hello, Students!"),
+//         onMouseEnter: () => console.log("Hello, Students!"),
+//         onMouseLeave: () => console.log("Bye, Students!"),
+//       }),
+//       React.createElement("button", {
+//         children: React.createElement("span", { children: "Click Me!" }),
+//       }),
+//       React.createElement("button", {
+//         children: React.createElement("span", { children: "Click Me!" }),
+//       }),
+//     ],
+//   })
+// );
