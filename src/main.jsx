736c970110632f2d3486812de2f3105e6b4b2aff
geremeev <g.eremeev@cft.ru>
2024-01-22 01:35:30 +0700

    last two restaurants

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <div>
+    
     <h1>{restaurants[0].name}</h1>
     <h3>Меню</h3>
     <ul>
@@ -30,6 +31,30 @@ root.render(
       <li>{restaurants[1].reviews[0].text}</li>
       <li>{restaurants[1].reviews[1].text}</li>
       <li>{restaurants[1].reviews[2].text}</li>
+    </ul>
+
+    <h1>{restaurants[2].name}</h1>
+    <h3>Меню</h3>
+    <ul>
+      <li>{restaurants[2].menu[0].name}</li>
+      <li>{restaurants[2].menu[1].name}</li>
+      <li>{restaurants[2].menu[2].name}</li>
+    </ul>
+    <h3>Отзывы</h3>
+    <ul>
+      <li>{restaurants[2].reviews[0].text}</li>
+    </ul>
+
+    <h1>{restaurants[3].name}</h1>
+    <h3>Меню</h3>
+    <ul>
+      <li>{restaurants[3].menu[0].name}</li>
+      <li>{restaurants[3].menu[1].name}</li>
+    </ul>
+    <h3>Отзывы</h3>
+    <ul>
+      <li>{restaurants[3].reviews[0].text}</li>
+      <li>{restaurants[3].reviews[1].text}</li>
     </ul>
 
   </div>
