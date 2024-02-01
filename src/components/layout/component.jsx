065ae70e9915c625bc12eeef556842780a1fa2a6
geremeev <g.eremeev@cft.ru>
2024-02-02 01:00:24 +0700

    lecture-3-hw just buttons in header

--- src/components/layout/component.jsx
+++ src/components/layout/component.jsx
@@ -1,10 +1,15 @@
 import { Footer } from "../footer/component";
 import { Header } from "../header/component";
+import {Button} from "../button/button.jsx";
 
 export const Layout = ({ children }) => {
     return (
         <div>
-            <Header headerText={"Dratuti"}/>
+            <Header>
+                <Button text={"darova ebat"}/>
+                <Button text={"zdarova zaebal"}/>
+                <Button text={"ebat' moi lisiy cherep kakie lyudi"}/>
+            </Header>
             <div>{children}</div>
             <Footer />
         </div>
