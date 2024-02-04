8ded312fac9d7012dfc6948d1e6e622293664463
geremeev <g.eremeev@cft.ru>
2024-02-04 23:29:53 +0700

    lecture-3/h2 linebreaks in code

--- src/components/counter/component.jsx
+++ src/components/counter/component.jsx
@@ -3,11 +3,19 @@ import {useState} from "react";
 
 export const Counter = () => {
 
-    const [counter, setCounter] = useState( 0 );
-    
+    const [counter, setCounter] = useState(0);
+
     return <span>
-        <Button text={"-"} onClick={() => setCounter(counter - 1)} isDisabled={counter <= 0}></Button>
+        <Button
+            text={"-"}
+            onClick={() => setCounter(counter - 1)}
+            isDisabled={counter <= 0}>
+        </Button>
         <span>{counter}</span>
-        <Button text={"+"} onClick={() => setCounter(counter + 1) } isDisabled={counter >= 5}></Button>
+        <Button
+            text={"+"}
+            onClick={() => setCounter(counter + 1)}
+            isDisabled={counter >= 5}>
+        </Button>
     </span>
 }
