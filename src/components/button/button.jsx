e7c549afaa4c95161935a04dc3880bb33d81e38e
geremeev <g.eremeev@cft.ru>
2024-02-05 00:00:41 +0700

    lecture-3/h2 styles wip

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,3 +1,17 @@
-export const Button = ({text, onClick, isDisabled}) => {
-    return <button onClick={onClick} disabled={isDisabled}>{text}</button>;
+import styles from "./styles.module.scss";
+import classNames from "classnames";
+
+const ButtonViewVariant = {
+    ["primary"]: styles.primary,
+    ["secondary"]: styles.secondary,
+}
+
+export const Button = ({text, onClick, isDisabled, viewVariant}) => {
+    return <button
+        className={classNames(ButtonViewVariant[viewVariant] || ButtonViewVariant["primary"])}
+        onClick={onClick}
+        disabled={isDisabled}
+    >
+        {text}
+    </button>;
 };
