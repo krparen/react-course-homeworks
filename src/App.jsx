6674d290a4a94fa5466ec5f4502a26b974dff553
geremeev <g.eremeev@cft.ru>
2024-02-06 00:58:51 +0700

    lecture-3/h2 best practices last part

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { Layout } from "./components/layout/component";
-import { restaurants } from "./constants/mock";
+import {RestaurantPage} from "./components/pages/restaurant-page/component.jsx";
+import {Layout} from "./components/layout/component.jsx";
 
 export const App = () => {
     return (
-        <Layout restaurants={restaurants}></Layout>
+        <Layout>
+            <RestaurantPage/>
+        </Layout>
+        
     );
 };
--- src/components/layout/component.jsx
+++ src/components/layout/component.jsx
@@ -1,24 +1,6 @@
-import { Footer } from "../footer/component";
-import { Header } from "../header/component";
-import {Restaurant} from "../restaurant/component.jsx";
-import {useState} from "react";
-import {RestaurantTabs} from "../restaurant-tabs/component.jsx";
+export const Layout = ({children}) => {
 
-export const Layout = ({ restaurants}) => {
-
-    const [activeRestaurantId, setActiveRestaurantId] = useState( restaurants[0].id );
-    
-    const activeRestaurant = restaurants.find(
-        ({id}) => id === activeRestaurantId
-    );
-    
     return (
-        <div>
-            <Header>
-                <RestaurantTabs restaurants={restaurants} onSelect={setActiveRestaurantId}/>
-            </Header>
-                <Restaurant restaurant={activeRestaurant}/>
-            <Footer />
-        </div>
+        <div>{children}</div>
     );
 };
