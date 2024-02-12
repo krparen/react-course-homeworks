4eb5cdc68bbb915efc0f3bcb8b9cfca07c21293a
geremeev <g.eremeev@cft.ru>
2024-02-13 01:42:29 +0700

    lecture-4/hw added styles to form

--- src/components/review-form/component.jsx
+++ src/components/review-form/component.jsx
@@ -1,13 +1,28 @@
+import styles from "./styles.module.scss";
+
 export const ReviewForm = () => {
-    return <div>
-        <h3>Обратная связь</h3>
-        <form>
-            <label htmlFor="name">Имя</label>
-            <input type="text" id="name"/>
-            <label htmlFor="text">Текст</label>
-            <input type="text" id="text"/>
-            <label htmlFor="rating">Рейтинг</label>
-            <input type="text" id="rating"/>
-        </form>
-    </div>
+    return (
+        <div>
+            <h3>Обратная связь</h3>
+            <form className={styles.root}>
+                <div className={styles.field}>
+                    <label htmlFor="name">Имя</label>
+                    <input type="text" id="name"/>
+                </div>
+                <div className={styles.field}>
+                    <label htmlFor="text">Текст</label>
+                    <input type="text" id="text"/>
+                </div>
+                <div className={styles.field}>
+                    <label htmlFor="rating">Рейтинг</label>
+                    <input
+                        type="number"
+                        id="rating"
+                        min={1}
+                        max={5}
+                    />
+                </div>
+            </form>
+        </div>
+    )
 }
