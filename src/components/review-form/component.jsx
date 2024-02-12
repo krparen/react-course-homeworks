e99457ea439d04faf32d2b586e0318e207f477d7
geremeev <g.eremeev@cft.ru>
2024-02-13 02:17:45 +0700

    lecture-4/hw added useReducer

--- src/components/review-form/component.jsx
+++ src/components/review-form/component.jsx
@@ -1,17 +1,66 @@
 import styles from "./styles.module.scss";
+import {useReducer} from "react";
 
 export const ReviewForm = () => {
+
+    const INITIAL_VALUE = {
+        name: "",
+        text: "",
+        rating: 1
+    };
+
+    // action: {type: string, payload: any}
+
+    const reducer = (state, {type, payload}) => {
+        switch (type) {
+            case "setName":
+                return {
+                    ...INITIAL_VALUE,
+                    name: payload
+                }
+            case "setText":
+                return {
+                    ...state,
+                    text: payload
+                }
+            case "setRating":
+                return {
+                    ...state,
+                    rating: payload
+                }
+            default:
+                return state;
+        }
+    }
+
+    const [form, dispatch] = useReducer(reducer, INITIAL_VALUE, () => {});
+    console.log(form);
+
     return (
         <div>
             <h3>Обратная связь</h3>
             <form className={styles.root}>
                 <div className={styles.field}>
                     <label htmlFor="name">Имя</label>
-                    <input type="text" id="name"/>
+                    <input
+                        type="text"
+                        id="name"
+                        value={form.name}
+                        onChange={ (event) =>
+                             dispatch({type: "setName", payload: event.target.value})
+                        }
+                    />
                 </div>
                 <div className={styles.field}>
                     <label htmlFor="text">Текст</label>
-                    <input type="text" id="text"/>
+                    <input
+                        type="text"
+                        id="text"
+                        value={form.text}
+                        onChange={ (event) =>
+                            dispatch({type: "setText", payload: event.target.value})
+                        }
+                    />
                 </div>
                 <div className={styles.field}>
                     <label htmlFor="rating">Рейтинг</label>
@@ -20,6 +69,10 @@ export const ReviewForm = () => {
                         id="rating"
                         min={1}
                         max={5}
+                        value={form.rating}
+                        onChange={ (event) =>
+                            dispatch({type: "setRating", payload: Number(event.target.value)})
+                        }
                     />
                 </div>
             </form>
