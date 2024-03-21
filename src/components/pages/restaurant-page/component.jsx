94d8f7b45695c3306c82fe137b9d5493912e3f7c
geremeev <g.eremeev@cft.ru>
2024-03-21 17:01:38 +0700

    lecture-5/hw some steps with context

--- src/components/pages/restaurant-page/component.jsx
+++ src/components/pages/restaurant-page/component.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useContext, useState} from "react";
 import {Header} from "../../header/component.jsx";
 import {RestaurantTabs} from "../../restaurant-tabs/component.jsx";
 import {Restaurant} from "../../restaurant/component.jsx";
@@ -6,23 +6,37 @@ import {Footer} from "../../footer/component.jsx";
 import {restaurants} from "../../../constants/mock.js";
 import {ReviewForm} from "../../review-form/component.jsx";
 import {Button} from "../../button/component.jsx";
+import {UserContext} from "../../../contexts/user.js";
 
 export const RestaurantPage = () => {
-    const [activeRestaurantId, setActiveRestaurantId] = useState( restaurants[0].id );
+    const [activeRestaurantId, setActiveRestaurantId] = useState(restaurants[0].id);
 
     const activeRestaurant = restaurants.find(
         ({id}) => id === activeRestaurantId
     );
 
+    const [currentUser, setCurrentUser] = useState({"name": "", "password": ""});
+
+    const defaultUser = {"name": "George", "password": "12345"}
+
     return (
-        <div>
-            <Header>
-                <button>Login</button>
-                <RestaurantTabs restaurants={restaurants} onSelect={setActiveRestaurantId}/>
-            </Header>
-            <Restaurant restaurant={activeRestaurant}/>
-            <ReviewForm/>
-            <Footer />
-        </div>
+        <UserContext.Provider value={currentUser}>
+            <div>
+                <Header>
+                    <Button text={"Login"} viewVariant={"login"}
+                            onClick={
+                                () => {
+                                    setCurrentUser(defaultUser);
+                                    console.log("user in context: ", currentUser)
+                                }
+                            }
+                    ></Button>
+                    <RestaurantTabs restaurants={restaurants} onSelect={setActiveRestaurantId}/>
+                </Header>
+                <Restaurant restaurant={activeRestaurant}/>
+                <ReviewForm/>
+                <Footer/>
+            </div>
+        </UserContext.Provider>
     );
 }
