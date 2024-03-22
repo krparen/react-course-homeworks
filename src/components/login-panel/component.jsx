f9a2689d308dbe3e15b1c2f8d9884610f53a99bb
geremeev <g.eremeev@cft.ru>
2024-03-22 16:22:15 +0700

    lecture-5/hw somehow works, but not smoothly

--- src/components/login-panel/component.jsx
+++ src/components/login-panel/component.jsx
@@ -3,10 +3,30 @@ import {useContext} from "react";
 import {UserContext} from "../../contexts/user.js";
 
 export const LoginPanel = ({onLogin, onLogout}) => {
-    //const currentUser = useContext(UserContext);
+    const currentUser = useContext(UserContext);
 
     return <div>
-        <Button text={"Login"} viewVariant={"login"} onClick={() => console.log("on login")}></Button>
-        <Button text={"Logout"} viewVariant={"login"} onClick={() => console.log("on logout")}></Button>
+        <Button
+            text={"Login"}
+            viewVariant={"login"}
+            onClick={
+                () => {
+                    onLogin();
+                    console.log("on login");
+                    console.log("current context user: ", currentUser);
+                }
+            }>
+        </Button>
+        <Button
+            text={"Logout"}
+            viewVariant={"login"}
+            onClick={
+                () => {
+                    onLogout();
+                    console.log("on logout");
+                    console.log("current context user: ", currentUser);
+                }
+            }>
+        </Button>
     </div>
 }
