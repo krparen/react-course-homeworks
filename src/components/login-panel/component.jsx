edab37f61945bfd3d8981f6de631f39ca15b22ae
geremeev <g.eremeev@cft.ru>
2024-03-27 01:14:32 +0700

    lecture-5/hw nanofix

--- src/components/login-panel/component.jsx
+++ src/components/login-panel/component.jsx
@@ -12,7 +12,7 @@ export const LoginPanel = () => {
             viewVariant={"login"}
             onClick={() => setCurrentUser(defaultUser)}>
         </Button>
-        {currentUser.name}
+        {currentUser?.name}
         <Button
             text={"Logout"}
             viewVariant={"login"}
