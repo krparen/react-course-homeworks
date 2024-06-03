0c55912ef7b6bd75e61a7104094530a8ba0a0edc
geremeev <g.eremeev@cft.ru>
2024-06-04 01:00:13 +0700

    lecture-5/hw modal centered

--- src/components/header/component.jsx
+++ src/components/header/component.jsx
@@ -1,5 +1,9 @@
 import {LoginPanel} from "../login-panel/component.jsx";
+import styles from "./styles.module.scss"
 
 export const Header = () => {
-    return <header><LoginPanel/></header>
+    return <header className={styles.root}>
+        Header
+        <LoginPanel/>
+    </header>
 };
