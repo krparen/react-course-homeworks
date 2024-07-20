74eb7d7ec512c9de89b27dad8feaa8f457cd5917
geremeev <g.eremeev@cft.ru>
2024-07-20 22:13:56 +0700

    lecture-7/hw somehow get reviews from server

--- src/components/restaurant/component.jsx
+++ src/components/restaurant/component.jsx
@@ -7,17 +7,30 @@ import {selectRestaurantById} from "../../redux/entities/restaurant/selectors.js
 import {useEffect, useState} from "react";
 import {selectIsLoading} from "../../redux/ui/request/index.js";
 import {getDishes} from "../../redux/entities/dish/thunks/get-dishes.js";
+import {getReviews} from "../../redux/entities/review/thunks/get-reviews.js";
 
 export const Restaurant = ({restaurantId}) => {
 
     const [requestId, setRequestId] = useState();
-    const isLoading = useSelector(
+    const [reviewRequestId, setReviewRequestId] = useState();
+    const isDishesLoading = useSelector(
         (state) => {
-            console.log("INSIDE DISH USE SELECTOR: current request id value: ", requestId);
+            console.log("INSIDE DISH USE SELECTOR FOR DISHES: current request id value: ", requestId);
             const isTrulyLoading = selectIsLoading(state, requestId);
-            console.log("INSIDE DISH USE SELECTOR: selectIsLoading on evaluation moment: ", isTrulyLoading);
+            console.log("INSIDE DISH USE SELECTOR FOR DISHES: selectIsLoading on evaluation moment: ", isTrulyLoading);
             const result = !requestId || isTrulyLoading;
-            console.log("INSIDE DISH USE SELECTOR: result of isLoading evaluation: ", result)
+            console.log("INSIDE DISH USE SELECTOR FOR DISHES: result of isDishesLoading evaluation: ", result)
+            return result;
+        }
+    );
+
+    const isReviewsLoading = useSelector(
+        (state) => {
+            console.log("INSIDE DISH USE SELECTOR FOR REVIEWS: current request id value: ", reviewRequestId);
+            const isTrulyLoading = selectIsLoading(state, reviewRequestId);
+            console.log("INSIDE DISH USE SELECTOR FOR REVIEWS: selectIsLoading on evaluation moment: ", isTrulyLoading);
+            const result = !reviewRequestId || isTrulyLoading;
+            console.log("INSIDE DISH USE SELECTOR FOR REVIEWS: result of isReviewsLoading evaluation: ", result)
             return result;
         }
     );
@@ -25,8 +38,13 @@ export const Restaurant = ({restaurantId}) => {
     const dispatch = useDispatch();
     useEffect(() => {
         const requestId = dispatch(getDishes()).requestId;
-        console.log("INSIDE DISH USE EFFECT request id: ", requestId);
+        console.log("INSIDE DISH USE EFFECT dishes request id: ", requestId);
         setRequestId(requestId);
+    }, [dispatch]);
+    useEffect(() => {
+        const requestId = dispatch(getReviews()).requestId;
+        console.log("INSIDE DISH USE EFFECT reviews request id: ", requestId);
+        setReviewRequestId(requestId);
     }, [dispatch]);
 
 
@@ -47,8 +65,12 @@ export const Restaurant = ({restaurantId}) => {
 
     console.log("INSIDE RESTAURANT COMPONENT:  denormalized reviews: ", denormalizedReviews);
     
-    if (isLoading) {
-        return (<div>Loading all dishes...</div>) ;
+    if (isDishesLoading) {
+        return (<div>Loading all dishes...</div>);
+    }
+    
+    if (isReviewsLoading) {
+        return (<div>Loading all reviews...</div>);
     }
 
     return (
--- src/redux/entities/review/index.js
+++ src/redux/entities/review/index.js
@@ -1,14 +1,14 @@
-import {createSlice} from "@reduxjs/toolkit";
-import {normalizedReviews} from "../../../constants/normalized-mock.js";
+import {createEntityAdapter, createSlice} from "@reduxjs/toolkit";
+import {getReviews} from "./thunks/get-reviews.js";
+
+const entityAdapter = createEntityAdapter();
 
 export const reviewSlice = createSlice({
     name: "review",
-    initialState: {
-        entities: normalizedReviews.reduce((acc, review) => {
-            acc[review.id] = review;
-            return acc;
-        }, {}),
-        
-        ids: normalizedReviews.map(({id}) => id)
-    }
+    initialState: entityAdapter.getInitialState(),
+    extraReducers: (builder) =>
+        builder.addCase(getReviews.fulfilled, (state, {payload}) => {
+            console.log("payload when getReviews fulfilled:", payload);
+            entityAdapter.setAll(state, payload);
+        }) 
 });
