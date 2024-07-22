964ee52c69d6c49a86cfca09edeaffb192a017d6
geremeev <g.eremeev@cft.ru>
2024-07-23 00:20:08 +0700

    lecture-7/hw cart works, but styles are questionable

--- src/components/layout/component.jsx
+++ src/components/layout/component.jsx
@@ -1,3 +1,5 @@
+import styles from "./styles.module.scss";
+
 export const Layout = ({children}) => {
 
     return (
@@ -6,6 +8,7 @@ export const Layout = ({children}) => {
             <div id="modal-container"/>
             <div id="popup-container"/>
             <div id="tooltip-container"/>
+            <div id="popoverContainer" className={styles.popoverContainer}/>
         </div>
     );
 };
